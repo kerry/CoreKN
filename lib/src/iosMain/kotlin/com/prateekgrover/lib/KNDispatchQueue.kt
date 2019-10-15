72b65a320ddec8fcbfe847fa324f7a514a9a92e6
Prateek Grover <prateek.grover@phonepe.com>
2019-10-15 12:56:33 +0530

    freezing crash reproduced

--- lib/src/iosMain/kotlin/com/prateekgrover/lib/KNDispatchQueue.kt
+++ lib/src/iosMain/kotlin/com/prateekgrover/lib/KNDispatchQueue.kt
@@ -1,7 +1,5 @@
 package com.prateekgrover.lib
 
-import kotlinx.cinterop.COpaquePointer
-import platform.posix.sleep
 import kotlin.native.concurrent.*
 
 actual class KNDispatchQueue {
@@ -10,30 +8,17 @@ actual class KNDispatchQueue {
         actual val sharedInstance: KNDispatchQueue = KNDispatchQueue()
     }
 
-    val observerWorker: Worker = Worker.start().freeze()
-    internal var workQueue: COpaquePointer? = DetachedObjectGraph(TransferMode.SAFE) { mutableListOf<KNWork>() }.asCPointer()
+    private var workQueue: MutableList<KNWork> = mutableListOf()
 
     init {
-        println("kndispatch queue initialized")
+        workQueue.ensureNeverFrozen()
     }
 
     actual fun performWorkAsync() {
-        // works with 1000 iterations but not with 2000
-        for (i in 1..2000) {
-            val task = {
-                val detachedWorkQueue = DetachedObjectGraph<MutableList<KNWork>>(workQueue).attach()
-                val success = detachedWorkQueue.add(KNWork(i) { sleep(2) })
-            }.freeze()
-            observerWorker.executeAfter(0, task)
-        }
+        ::performWork.freeze()
+    }
 
-        // running this multiple times causes problems
-//        for (i in 1..10000) {
-//            observerWorker.execute(TransferMode.SAFE, {
-//                workQueue
-//            }) {
-//                val detachedWorkQueue = DetachedObjectGraph<MutableList<KNWork>>(it).attach()
-//            }
-//        }
+    fun performWork() {
+
     }
 }
